614dc9167e0cbdda73e0331c38b0775184044248
Arr0zado <55147716+arrzdev@users.noreply.github.com>
2021-03-26 01:24:22 +0000

    try generation

--- master/main.py
+++ master/main.py
@@ -61,25 +61,19 @@ def game(current_move='KEY_RIGHT'):
                 food[1] -= 1
 
 
-        #GET AVAILABLE MOVES
-        available_moves = get_available_moves(current_move)
-
-        #DECIDE THE BEST MOVE JUST RELATIVE TO THE FRUIT
+        #CALCULATE RELATIVE DISTANCES, BETWEEN FOOD AND SNAKE HEAD
         distanceX = food[1] - snake[0][1]
         distanceY = food[0] - snake[0][0]
 
-        fruit_best_move = get_best_move_fruit(dx=distanceX, dy=distanceY, current_move=current_move)
 
-        #CREATES THE NEW HEAD TO THAT MOVE
-        fruit_move_head = [snake[0][0] + (fruit_best_move == KEY_DOWN and 1) + (fruit_best_move == KEY_UP and -1), snake[0][1] + (fruit_best_move == KEY_LEFT and -1) + (fruit_best_move == KEY_RIGHT and 1)]
+        #GET AVAILABLE MOVES
+        available_moves = get_available_moves(current_move)
 
-        #DEFINE TEMP BEST MOVE FROM "FRUIT_BEST_MOVE"
-        best_move = fruit_best_move
+        #GET BEST MOVE
+        best_move = get_best_move(current_move=current_move, available=available_moves, dx=distanceX, dy=distanceY)
 
-        #IF THE FRUIT BEST MOVE HEAD IS A KILLABLE PLAY OR 6 CHUNKS AWAY FROM DIEING
-        if fruit_move_head in snake or get_free_space(move=fruit_best_move) < random.choice([5,6,7,8,9,10]):
-            best_move = get_best_move_sensors(available=available_moves)
-            
+
+        #PLAY    
         choosen = str(best_move).replace("259", "up").replace("260", "left").replace("261", "right").replace("258", "down")
 
         available_translated = []
@@ -143,8 +137,8 @@ def get_available_moves(current_move):
 
     return available_moves
 
-def get_best_move_fruit(dx=0, dy=0, current_move=''):
-    
+def get_best_move(current_move='', available=[], dx=0, dy=0):
+    #SET INITIAL BEST_MOVE
     best_move = current_move
 
     #PLAY DIAGONAL
@@ -194,70 +188,75 @@ def get_best_move_fruit(dx=0, dy=0, current_move=''):
             else:
                 best_move = random.choice([KEY_DOWN, KEY_UP])
 
-    ###
-    if get_free_space(move=best_move) < 10:
-        #HORIZONTAL
-        if dy == 0:
-            if dx < 0: #fruta a esquerda
-                if current_move != KEY_RIGHT:
-                    best_move = KEY_LEFT
-                else:
-                    best_move = random.choice([KEY_DOWN, KEY_UP])
-            else: #fruta a direita
-                if current_move != KEY_LEFT:
-                    best_move = KEY_RIGHT
-                else:
-                    best_move = random.choice([KEY_DOWN, KEY_UP])
-        elif dy < 0: #fruta em cima
-            if current_move != KEY_DOWN:
-                best_move = KEY_UP
-            else:
-                best_move = random.choice([KEY_RIGHT, KEY_LEFT])
-        else: #fruta em baixo
-            if current_move != KEY_UP:
-                best_move = KEY_DOWN
-            else:
-                best_move = random.choice([KEY_RIGHT, KEY_LEFT])
+    #CREATES THE NEW HEAD TO THAT MOVE
+    fruit_move_head = [snake[0][0] + (best_move == KEY_DOWN and 1) + (best_move == KEY_UP and -1), snake[0][1] + (best_move == KEY_LEFT and -1) + (best_move == KEY_RIGHT and 1)]
+
+    #IF THE FRUIT BEST MOVE HEAD IS A KILLABLE PLAY OR 6 CHUNKS AWAY FROM DIEING
+    if fruit_move_head in snake or get_free_space(move=best_move) < random.choice([3,4,5,6,7,8,9,10]):
+        max_score = 0
+        
+        #SET INITIAL MOVE
+        best_move = current_move
+
+        for first_moves in available:
+            father_score = 0
+            #1GENERATION
+            father_score += get_free_space(first_moves)
+
+            #2GENERATION
+            second_available = get_available_moves(first_moves)
+            for second_moves in second_available:
+                father_score += get_free_space(second_moves)
+
+                #3GENERATION
+                third_available = get_available_moves(second_moves)
+                for third_moves in third_available:
+                    father_score += get_free_space(third_moves)
+
+                    #4GENERATION
+                    fourth_available = get_available_moves(third_moves)
+                    for fourth_moves in fourth_available:
+                        father_score += get_free_space(fourth_moves)
+
+                        #5GENERATION
+                        fifth_available = get_available_moves(fourth_moves)
+                        for fifth_moves in fifth_available:
+                            father_score += get_free_space(fifth_moves)
+
+                            #6GENERATION
+                            six_available = get_available_moves(fifth_moves)
+                            for six_moves in six_available:
+                                father_score += get_free_space(six_moves)
+
+                                #7GENERATION
+                                seven_available = get_available_moves(six_moves)
+                                for seven_moves in seven_available:
+                                    father_score += get_free_space(seven_moves)
+
+                                    #8GENERATION
+                                    eight_available = get_available_moves(seven_moves)
+                                    for eight_moves in eight_available:
+                                        father_score += get_free_space(eight_moves)
+
+                                        #9GENERATION
+                                        nine_available = get_available_moves(eight_moves)
+                                        for nine_moves in  nine_available:
+                                            father_score += get_free_space(nine_moves)
+
+                                            #10GENERATION
+                                            ten_available = get_available_moves(nine_moves)
+                                            for ten_moves in  ten_available:
+                                                father_score += get_free_space(ten_moves)
+
+                            #COMPARE SCORES
+                            if father_score > max_score:
+                                max_score = father_score
+                                best_move = first_moves
+
+
+
 
     return best_move
-
-def get_best_move_sensors(available=[]):
-    max_score = 0
-
-    #choose a random move at beggining
-    best_move = random.choice(available)
-
-    for spec_move in available:
-        alive_score = get_free_space(spec_move)
-
-        if alive_score > max_score:
-            max_score = alive_score
-            best_move = spec_move
-
-    return best_move
-
-def get_fruit_distance(move=''):
-    distance = 0
-
-    possible_head = [snake[0][0] + (move == KEY_DOWN and 1) + (move == KEY_UP and -1), snake[0][1] + (move == KEY_LEFT and -1) + (move == KEY_RIGHT and 1)]
-
-    while possible_head != food and distance < (window.getmaxyx()[1] and window.getmaxyx()[0]): #se nao for instant death
-        #after passing alive check adds 1 to alive state
-        distance += 1
-
-        possible_head = [possible_head[0] + (move == KEY_DOWN and 1) + (move == KEY_UP and -1), possible_head[1] + (move == KEY_LEFT and -1) + (move == KEY_RIGHT and 1)]
-            
-        #TELEPORT THE HEAD IF IN THE BOARD LINES
-        if possible_head[0] == 0:
-            possible_head = [window.getmaxyx()[0]-1,possible_head[1]]
-        elif possible_head[0] == window.getmaxyx()[0]:
-            possible_head = [0, possible_head[1]]
-        elif possible_head[1] == 0:
-            possible_head = [possible_head[0], window.getmaxyx()[1]-1]
-        elif possible_head[1] == window.getmaxyx()[1]:
-            possible_head = [possible_head[0], 0]
-
-    return distance
 
 def check_if_killed():
     if snake[0] in snake[1:]: 
@@ -277,14 +276,7 @@ def get_free_space(move=''):
         possible_head = [possible_head[0] + (move == KEY_DOWN and 1) + (move == KEY_UP and -1), possible_head[1] + (move == KEY_LEFT and -1) + (move == KEY_RIGHT and 1)]
         
         #TELEPORT THE HEAD IF IN THE BOARD LINES
-        if possible_head[0] == 0:
-            possible_head = [window.getmaxyx()[0]-1, possible_head[1]]
-        elif possible_head[0] == window.getmaxyx()[0]:
-            possible_head = [0, possible_head[1]]
-        elif possible_head[1] == 0:
-            possible_head = [possible_head[0], window.getmaxyx()[1]-1]
-        elif possible_head[1] == window.getmaxyx()[1]:
-            possible_head = [possible_head[0], 0]
+        possible_head = teleport_snake_head(head=possible_head)
 
     return alive
 
@@ -356,7 +348,7 @@ if __name__ == '__main__':
     spawn_y = random.randint(1, window.getmaxyx()[0]-2)
 
     #build snake based on the head coordinates
-    snake = [[spawn_y, spawn_x], [spawn_y, spawn_x-1], [spawn_y, spawn_x-2]]
+    snake = [[spawn_y, spawn_x], [spawn_y, spawn_x-1], [spawn_y, spawn_x-2],[spawn_y, spawn_x-3],[spawn_y, spawn_x-4],[spawn_y, spawn_x-5],[spawn_y, spawn_x-6],[spawn_y, spawn_x-7],[spawn_y, spawn_x-8],[spawn_y, spawn_x-9],[spawn_y, spawn_x-10],[spawn_y, spawn_x-11],[spawn_y, spawn_x-12],[spawn_y, spawn_x-13], [spawn_y, spawn_x-14], [spawn_y, spawn_x-15],[spawn_y, spawn_x-16],[spawn_y, spawn_x-17],[spawn_y, spawn_x-18],[spawn_y, spawn_x-19],[spawn_y, spawn_x-20],[spawn_y, spawn_x-21],[spawn_y, spawn_x-22]]
 
 
 
